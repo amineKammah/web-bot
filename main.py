36e5d61dc3598889b287cc76897121671a11243f
Amine Kammah <amine.kammah@sage.com>
2021-02-27 11:08:45 +0100

    Refactor main

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from create_account import *
-from boost_link import *
 import threading
-from utils import get_boost_link, set_location_cookie
+from utils import set_location_cookie, get_driver
 from proxy import fill_proxies_queue, get_proxy
 import logging
 import warnings
@@ -13,7 +12,7 @@ def create_accounts():
         logging.info("Getting proxy")
         proxy = get_proxy()
         logging.info(f"Got proxy, {proxy}")
-        for _ in range(100):
+        for _ in range(10):
             try:
                 driver = get_driver(proxy)
                 register_link = "https://login.aliexpress.com/"
@@ -29,12 +28,9 @@ def create_accounts():
                 logging.warning(e)
                 break
 
+
 for _ in range(5):
     threading.Thread(target=create_accounts).start()
 
 fill_proxies_queue(1_000)
 
-
-# boost_link = get_boost_link()
-# driver.get(boost_link)
-# buddy_bonus(driver, email, password)
